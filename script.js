7a40cc9d820820006fa77d63cee49ee82b6247d6
TJ Santos <tjscode@outlook.com>
2016-11-14 18:50:24 -0800

    Working timer

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 let minutes = document.querySelector('#minutes');
+let seconds = document.querySelector('#seconds');
+let millis = document.querySelector('#millis');
 minutes.value = 10;
-let reminderButton = document.querySelector('#reminderButton');
-let active = false;
 
+let frameId = 0;
+let endTime;
+
+function render(timestamp) {
+    "use strict";
+    let timeLeft = endTime - Date.now();
+    minutes.value = Math.floor(timeLeft / 60000);
+    let millisLeft = timeLeft % 60000;
+    seconds.value = Math.floor(millisLeft / 1000);
+    millis.value = millisLeft % 1000;
+    frameId = requestAnimationFrame(render);
+}
 function activate() {
-  active = true;
-  reminderButton.innerText = 'pause';
+    frameId = requestAnimationFrame(render);
+    let minutesVal = parseInt(minutes.value, 10) || 0;
+    let secondsVal = parseInt(seconds.value, 10) || 0;
+    let millisVal = parseInt(millis.value, 10) || 0;
+    console.log("started", minutesVal, secondsVal, millisVal);
+    endTime = Date.now() + (minutesVal*60000 + secondsVal*1000 + millisVal);
+    reminderButton.innerText = 'pause';
 }
 function deactivate() {
-  active = false;
-  reminderButton.innerText = 'start';
+    console.log("paused", minutes.value, seconds.value, millis.value);
+    cancelAnimationFrame(frameId);
+    frameId = 0;
+    reminderButton.innerText = 'start';
 }
 function toggleActive() {
-  active ? deactivate() : activate();
+    frameId ? deactivate() : activate();
 }
+let reminderButton = document.querySelector('#reminderButton');
 reminderButton.addEventListener('click', toggleActive);
